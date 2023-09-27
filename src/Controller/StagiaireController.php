111a13c3458f5e63054431b92192f0722d47ffdf
derdeche <foued.derdeche@gmail.com>
2023-09-27 16:31:28 +0200

    modif function

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Stagiaire;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class StagiaireController extends AbstractController
 {
     #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $stagiaire = $entityManager->getRepository(Stagiaire::class)->findBy([], ['nom' => 'ASC']);
         return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
+            'stagiaire' => $stagiaire,
         ]);
     }
 }
