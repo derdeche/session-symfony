e0668ba2db4f0a0efc835174fa2f3830ef9cd17f
derdeche <foued.derdeche@gmail.com>
2023-09-27 16:30:57 +0200

    modif function

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Module;
+// use App\Controller\SessionController;
+// use App\Repository\SessionRepository;
+use App\Entity\Session;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    
+
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $sessions = $entityManager->getRepository(Session::class)->findAll();
+        
+        
+
+        $module = $entityManager->getRepository(Module::class)->findAll();
+        
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions,
+            'module' => $module,
         ]);
     }
 }
+
