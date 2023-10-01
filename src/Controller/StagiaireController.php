b1acb9818aa92bfdbae1b189a351dca8ef853196
derdeche <foued.derdeche@gmail.com>
2023-10-01 23:46:27 +0200

    function details stagiaire

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,20 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Session;
 use App\Entity\Stagiaire;
+use App\Repository\StagiaireRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
     #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(StagiaireRepository $stagiaireRepository): Response
     {
-        $stagiaire = $entityManager->getRepository(Stagiaire::class)->findBy([], ['nom' => 'ASC']);
+        $stagiaire = $stagiaireRepository->findBy([], ['nom' => 'ASC']);
         return $this->render('stagiaire/index.html.twig', [
             'stagiaire' => $stagiaire,
         ]);
+    }
+
+    #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
+    public function show(Stagiaire $stagiaire):Response
+
+    {
+        
+        return $this->render('stagiaire/show.html.twig', [
+            'stagiaire' => $stagiaire,
+                      
+         ]);
     }
 }
