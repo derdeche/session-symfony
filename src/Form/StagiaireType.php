3667c7adb0d64e28b31f792afa1ceefd6be3d5ba
derdeche <foued.derdeche@gmail.com>
2023-10-02 16:59:13 +0200

    creation

---
+++ src/Form/StagiaireType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class StagiaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom',TextType::class)
+            ->add('prenom',TextType::class)
+            ->add('ville',TextType::class)
+            ->add('telephone',TextType::class)
+            ->add('courriel',TextType::class)
+            ->add('dateNaissance',DateType::class)
+            ->add('sessions',EntityType::class
+            ,[
+                'class'=>Session::class,
+                'choice_label'=>'session',
+                'multiple'=>true,
+                'expanded'=>true,
+                'by_reference'=>false,
+            ]
+            )
+
+            ->add('valider',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
